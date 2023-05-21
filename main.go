851ddf39904ae014255858ec5137e5a259ca1374
surajs <3ssalunke>
2023-05-21 20:20:15 +0530

    restored main.go for master branch

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 	"github.com/3ssalunke/gopoker/p2p"
 )
 
-func makeServerAndStart(addr string, apiAddr string) *p2p.Node {
+func makeServerAndStart(addr string, apiAddr string) *p2p.Server {
 	cfg := p2p.ServerConfig{
 		Version:       "GPOKER V0.1-alpha",
 		ListenAddr:    addr,
 		ApiListenAddr: apiAddr,
 		GameVariant:   p2p.TexasHoldem,
 	}
-	server := p2p.NewNode(cfg)
+	server := p2p.NewServer(cfg)
 	go server.Start()
 
 	time.Sleep(time.Second * 1)
@@ -23,29 +23,20 @@ func makeServerAndStart(addr string, apiAddr string) *p2p.Node {
 }
 
 func main() {
-	node1 := makeServerAndStart(":3000", ":3001")
-	node2 := makeServerAndStart(":4000", ":4001")
-
-	err := node2.Connect(node1.ListenAddr)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// playerA := makeServerAndStart(":3000", ":3001")
-	// playerB := makeServerAndStart(":4000", ":4001")
-	// playerC := makeServerAndStart(":5000", ":5001")
+	playerA := makeServerAndStart(":3000", ":3001")
+	playerB := makeServerAndStart(":4000", ":4001")
+	playerC := makeServerAndStart(":5000", ":5001")
 	// playerD := makeServerAndStart(":6000")
 	// playerE := makeServerAndStart(":7000")
 
-	// if err := playerB.Connect(playerA.ListenAddr); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// time.Sleep(time.Second * 1)
-	// if err := playerC.Connect(playerB.ListenAddr); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// time.Sleep(time.Second * 1)
+	if err := playerB.Connect(playerA.ListenAddr); err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Second * 1)
+	if err := playerC.Connect(playerB.ListenAddr); err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(time.Second * 1)
 	// if err := playerD.Connect(playerA.ListenAddr); err != nil {
 	// 	log.Fatal(err)
 	// }
